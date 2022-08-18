0b8be80b33ada86a32c1842110a117710a22a732
Echoframe <49283800+Echoframe@users.noreply.github.com>
2022-08-18 11:50:39 +0200

    Added english locale

---
+++ locales/en.lua
@@ -0,0 +1,19 @@
+Locales ['en'] = {
+    --untested
+  ['uran_pickupprompt'] = 'Press ~INPUT_CONTEXT~ to pick up a ~o~Safety Box~s~.',
+  ['uran_fullprompt'] = 'You can`t carry more of these!',
+  ['uran_processed'] = 'Opened ~o~Safety Box~s~.',
+  ['uran_processingstarted'] = 'Process started...',
+  ['uran_processingtoofar'] = 'Process not possible.',
+  ['uran_beginunpacking'] = 'Drücke ~INPUT_CONTEXT~ um den Sicherheitsbehälter zu öffnen.',
+  ['uran_unpackTime'] = 'Please wait 20 seconds.',
+  ['uran_tooFar'] = 'You are too far away.',
+  ['uran_processed_2'] = 'You don`t have enough material.',
+  ['processDirtyUranium'] = 'Press ~INPUT_CONTEXT~ to clean the Uranium.',
+  ['pressUranium'] = 'Press ~INPUT_CONTEXT~ to process the Uranium.',
+  ['uran_cleaned'] = 'You cleaned the Uranium.',
+  ['uran_teleport'] = 'Press ~INPUT_CONTEXT~ to call the elevator.',
+  ['uran_sell'] = 'Press ~INPUT_CONTEXT~ to sell the fuel rods.',
+  ['uran_sold'] = 'Press ~INPUT_CONTEXT~ to sell the fuel rods.',
+  }
+
